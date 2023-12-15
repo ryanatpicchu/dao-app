e86622bded5068279ab0d3e9e633c95d4039f8b5
ryanatpicchu <ryan@picchu.io>
2023-12-15 22:53:58 +0800

    translations

--- src/legos/forms.ts
+++ src/legos/forms.ts
@@ -8,9 +8,9 @@ const PROPOSAL_SETTINGS_FIELDS = [FIELD.PROPOSAL_EXPIRY, FIELD.PROP_OFFERING];
 export const APP_FORM: Record<string, CustomFormLego> = {
   TEST_FORM: {
     id: "TEST_FORM",
-    title: "Super Signal Form",
-    subtitle: "Super Signal Proposal",
-    description: "Ratify on-chain using a DAO proposal.",
+    title: "超級信號表格",
+    subtitle: "超級信號提案",
+    description: "利用DAO提案上鏈作為簽屬",
     requiredFields: { title: true, description: true, testField: true },
     log: true,
     tx: APP_TX.TEST_TX,
